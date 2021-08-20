542df50d00a59d61e572ee72fe9fcb6ca1de24da
aezakmi007 <abdullahfarhan207@gmail.com>
2021-08-20 20:14:55 +0530

    React-router initialised

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pageTwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
