48a72d29c43c3b9439d99f9dc66114ad721b2aa2
aezakmi007 <abdullahfarhan207@gmail.com>
2021-09-02 16:10:13 +0530

    Listed all streams

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,11 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import _ from "lodash";
 export default (state = {}, action) => {
   switch (action.type) {
+    case "FETCH_STREAMS":
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case "FETCH_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "CREATE_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "EDIT_STREAM":
       return { ...state, [action.payload.id]: action.payload };
+    case "DELTE_STREAM":
+      return _.omit(state, action.payload);
     default:
       return state;
   }
